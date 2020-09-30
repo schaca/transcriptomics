72f1b33b3d1e43a8a5c0d4753bb959ba114b27d7
schaca <ozancift@gmail.com>
2020-09-30 15:01:53 +0200

    Update help

--- deinterleave.sh
+++ deinterleave.sh
@@ -38,8 +38,8 @@ do
 	echo "  [-i|--indir]	Specify the path to interleaved fastq files"
         echo "  [-o|--outdir]	Specify a name for output directory"
 	echo " "
-	echo "	This script splits interleaved fastq reads into two paired-end files. It will process all compressed fastq files in input directory, so input path should only contain interleaved fastq files."
-	echo "	It removes the pairing info on the sequence identifier lines using awk (everything after '/') and outputs two paired-end fastq files with extensions '.1.fq.gz' and '.2.fq.gz'."
+	echo "	This script splits interleaved fastq reads into two paired-end files. All compressed fastq files in input directory are processed, so input path should only contain interleaved fastq files."
+	echo "	It removes the pairing info on the sequence identifier lines using awk (following interleave.sh, everything after '\' are removed), and outputs two paired-end fastq files with extensions '...1.fq.gz' and '...2.fq.gz'."
 	echo "	The part of the code for splitting the reads is modified from: https://gist.github.com/nathanhaigh/3521724"
         exit 0
         ;;
@@ -67,16 +67,15 @@ cd deinterleave_tmp/
 echo "$(date)" | tee deinterleave.log
 echo "Bash version: ${BASH_VERSION}" | tee -a deinterleave.log
 for file in ../$INDIR/*.fq.gz; do
-	echo "... " | tee -a deinterleave.log
-	echo "Splitting reads for sample: $identifier" | tee -a deinterleave.log
 	filename=$(basename $file)
 	identifier=$(basename $file | cut -d "." -f1)
+	echo "..." | tee -a deinterleave.log
+	echo "Splitting reads: $identifier" | tee -a deinterleave.log
 	gunzip -c $file > "${filename%.gz}"
 	cat "${filename%.gz}" | awk 'NR % 4 == 1 {split($0,a,/[/]/);$0=a[1]}1' | paste - - - - - - - - | tee | cut -f 1-4 | tr "\t" "\n" | egrep -v '^$' | gzip -1 > "${identifier}".corr.1.fq.gz
 	cat "${filename%.gz}" | awk 'NR % 4 == 1 {split($0,a,/[/]/);$0=a[1]}1' | paste - - - - - - - - | tee | cut -f 5-8 | tr "\t" "\n" | egrep -v '^$' | gzip -1 > "${identifier}".corr.2.fq.gz 
 	n_reads=$(zcat "${identifier}".corr.1.fq.gz | awk 'NR % 4 == 1 {print $0}' | wc -l)
 	echo "$n_reads paired-end reads were split into two fastq files"| tee -a deinterleave.log
-	echo "... " | tee -a deinterleave.log
 done
 
 # Copy files and remove temporary working directory 
@@ -85,6 +84,7 @@ cp *.corr.2.fq.gz ../"$OUTDIR"
 cp deinterleave.log ../"$OUTDIR"
 cd ../
 rm -rf deinterleave_tmp 
+echo "..." | tee -a "$OUTDIR"/deinterleave.log
 echo "Output files are saved to directory: $OUTDIR" | tee -a "$OUTDIR"/deinterleave.log
 
 
